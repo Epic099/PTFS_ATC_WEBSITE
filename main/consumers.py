d692effb5eae0591997078a55dfb26f54d6e805e
Epic099Scripting <Speedyben09991@gmail.com>
2023-09-23 07:37:08 +0200

    1

---
+++ main/consumers.py
@@ -0,0 +1,22 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+
+class AtcConsumer(WebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = "test_room"
+        
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        await self.accept()
+        
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        print("Disconnected", code)
