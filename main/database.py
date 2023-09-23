c39be6c56b790475fcb226c48926beba772ae4bb
Epic099 <111688261+Epic099@users.noreply.github.com>
2023-09-23 07:27:00 +0200

    Update database.py

--- main/database.py
+++ main/database.py
@@ -38,8 +38,7 @@ class Server():
         
         
         #Implement Other Charts types later
-        #return Charts(ground_files, airway_charts)
-        return Charts(ground_files, None)
+        return Charts(ground_files, airway_charts)
     def runway_valid(icao : str, runway : str):        
         runways = Airport_Data["Charts_Data"][icao]["Runways"]
         return runway in runways
